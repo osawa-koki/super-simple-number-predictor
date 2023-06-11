ec8ce68fb082101180a6b34408d6cbbf3f9cca90
osawa-koki <94336223+osawa-koki@users.noreply.github.com>
2023-06-11 17:38:37 +0000

    判定まで。

--- predict-test.sh
+++ predict-test.sh
@@ -1,6 +1,10 @@
 
 for i in `seq 2 4`
 do
-  json=`curl -X POST -F image=@sample-numbers/$i.png http://localhost:8000/api/numeric-judge`
-  echo $json | jq '.\""'
+  probability=`curl --silent -X POST -F image=@sample-numbers/$i.png http://localhost:8000/api/numeric-judge | jq ".\"$i\""`
+  if test $probability -gt 70; then
+    echo -e "\e[32mOK\e[m $i -> $probability%"
+  else
+    echo -e "\e[31mNG\e[m $i -> $probability%"
+  fi
 done
