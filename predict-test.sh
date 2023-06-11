323064c45bb90b4d6a4ee62310ee9c4d9915db5b
osawa-koki <94336223+osawa-koki@users.noreply.github.com>
2023-06-11 17:45:01 +0000

    判定スクリプトまで完成させた。

--- predict-test.sh
+++ predict-test.sh
@@ -1,10 +1,22 @@
 
+success_bit=true
+success_threshold=70
+
 for i in `seq 2 4`
 do
   probability=`curl --silent -X POST -F image=@sample-numbers/$i.png http://localhost:8000/api/numeric-judge | jq ".\"$i\""`
-  if test $probability -gt 70; then
+  if test $probability -gt $success_threshold; then
     echo -e "\e[32mOK\e[m $i -> $probability%"
   else
     echo -e "\e[31mNG\e[m $i -> $probability%"
+    success_bit=false
   fi
 done
+
+if $success_bit; then
+  echo -e "\e[32m- SUCCESS!!!\e[m"
+  exit 0
+else
+  echo -e "\e[31m- FAILED...\e[m"
+  exit 1
+fi
