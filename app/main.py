6b22f9a40b23bf8204604263727951d62d5be671
osawa-koki <github@osawa.dev>
2023-06-11 22:01:14 +0900

    tmp

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 import uuid
 
-# from predict import predict
+from predict import predict
 
 app = FastAPI()
 
@@ -21,29 +21,29 @@ app.add_middleware(
 def health():
     return {"status": "ok"}
 
-# @app.post("/api/numeric-judge")
-# def receive_image(image: bytes = File(...)):
-#     # GUIDを生成する。
-#     guid = uuid.uuid4()
-#     # GUIDを小文字に変換する。
-#     guid = guid.hex.lower()
+@app.post("/api/numeric-judge")
+def receive_image(image: bytes = File(...)):
+    # GUIDを生成する。
+    guid = uuid.uuid4()
+    # GUIDを小文字に変換する。
+    guid = guid.hex.lower()
 
-#     # ファイル名を定義する。
-#     filename = f"./tmp/{guid}.png"
+    # ファイル名を定義する。
+    filename = f"./tmp/{guid}.png"
 
-#     # 画像を保存する。
-#     with open(filename, "wb") as f:
-#         f.write(image)
+    # 画像を保存する。
+    with open(filename, "wb") as f:
+        f.write(image)
 
-#     # 画像を予測する。
-#     result = predict(filename)
+    # 画像を予測する。
+    result = predict(filename)
 
-#     # 予測結果を返す。
-#     return result
+    # 予測結果を返す。
+    return result
 
-# # Serve static files from the "www" directory and set index.html as the default file
-# app.mount("/api", app)
-# app.mount("/", StaticFiles(directory="www", html=True), name="www")
+# Serve static files from the "www" directory and set index.html as the default file
+app.mount("/api", app)
+app.mount("/", StaticFiles(directory="www", html=True), name="www")
 
 if __name__ == "__main__":
     import uvicorn
